e7e89b919ca3b8efd59ab356571e85d0df2b7c0e
Alikhan <alikhan.tursunbekov@gmail.com>
2023-11-02 12:55:01 +0600

    Initial commit

---
+++ Neobis_iOS_WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neobis_iOS_WeatherApp
+//
+//  Created by Alikhan Tursunbekov on 2/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
